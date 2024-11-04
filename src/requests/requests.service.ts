97acd1092b9d1728c1e2af215fc9a9f68262ba4e
Banana Duck <a.usama@example.com>
2024-11-04 20:10:47 +0300

    requests: fix

--- src/requests/requests.service.ts
+++ src/requests/requests.service.ts
@@ -1,7 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Request } from './requests.model';
-import { Repository } from 'typeorm';
+import { Not, Repository } from 'typeorm';
 import { Technician } from 'src/technicians/technicians.model';
 import { Status } from 'src/statuses/statuses.model';
 
@@ -14,28 +14,35 @@ export class RequestsService {
         private technicianRepo: Repository<Technician>,
         @InjectRepository(Status)
         private statusRepo: Repository<Status>
-    ) {}
+    ) { }
 
     async findAll() {
-        return await this.requestRepo.find({relations: ['status', 'technician'], order: {requested_at: 'DESC'}});
+        return await this.requestRepo.find({
+            relations: ['status', 'technician'],
+            where: [
+                { status: { id: 1 } }
+            ],
+            order: { requested_at: 'DESC' }
+        });
     }
+
 
     async find(id: number) {
-        return await this.requestRepo.findOneBy({id: id})
+        return await this.requestRepo.findOneBy({ id: id })
     }
 
-    async update(id: number, t: {technician: string}) {
+    async update(id: number, t: { technician: string }) {
         const tech = await this.technicianRepo.findOne({ where: { name: t.technician } });
         if (!tech) {
             throw new HttpException('technicion not found', HttpStatus.NOT_FOUND);
         }
 
-        const status = await this.statusRepo.findOneBy({value: 'в работе'})
+        const status = await this.statusRepo.findOneBy({ value: 'в работе' })
         if (!status) {
             throw new HttpException('status not found', HttpStatus.NOT_FOUND);
         }
 
 
-        await this.requestRepo.update({id: id}, {technician: tech, status: status})
+        await this.requestRepo.update({ id: id }, { technician: tech, status: status })
     }
 }
